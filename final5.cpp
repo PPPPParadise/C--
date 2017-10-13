755360c236242c18ca6951afa131093f7c734167
Shenghua(shawn) <shawndsh@gmail.com>
2017-10-14 00:35:44 +0200

    5

---
+++ final5.cpp
@@ -0,0 +1,32 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+using namespace std;
+int main() {
+ map<string,int> Fruitmap;
+ ifstream dataFile("list.txt");
+ string curLine;
+ string fruit;
+ while (getline(dataFile,curLine)) {
+  stringstream curSS(curLine);
+  curSS>>fruit;
+  map<string,int>::iterator iter;
+  iter=Fruitmap.find(fruit);
+  if (iter != Fruitmap.end()) {
+  Fruitmap[fruit]+=1;
+  }
+  else
+  {pair<string,int> other(fruit,1);
+  Fruitmap.insert(other);}
+ }
+ for (map<string,int>::iterator mapIter = Fruitmap.begin(); mapIter != Fruitmap.end(); ++mapIter) {
+  cout<<(*mapIter).first<<" "<<(*mapIter).second;
+  cout<<endl;
+ }
+
+ return 0;
+}
